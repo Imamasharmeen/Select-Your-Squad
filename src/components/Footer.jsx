b37923f3655917c290c2a9930d7951c725f8da76
Imamasharmeen <imamasharmeen@gmail.com>
2024-10-27 09:16:45 +0200

    Modifyed newsletter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,16 @@
 
 export default function Footer() {
   return (
-  <div className="absolute-full mx-auto  border border-red-600">
-    <div className="relative top-20 border-4  border-green-500">
+  <div className="absolute-full mx-auto  border border-yellow-500">
+    <div className="relative top-14 md:top-24 lg:top-40 border-4  border-green-500">
       
-      <div className="static bottom-10  w-10/12 md:w-9/12 lg:w-8/12 mx-auto p-5 text-center rounded-2xl bg-[#00000009] overlay border border-red-600">
-        <div className="p-5 md:p-12 lg:p-24 rounded-2xl bg-gradient-to-tr from-[#CAEDF7] via-[#FFFFFF] to-[#FADCA1] border border-blue-500">
-          <div className="w-1/2 mx-auto text-center rounded-2xl border border-red-600">
-          
-          
-            <h1 className="text-lg md:text-2xl lg:text-3xl font-bold">Subscribe to our Newsletter</h1>
-            <p className="py-4 text-base md:text-lg lg:text-xl">Get the latest updates and news right in your inbox!</p>
-            <div className="flex justify-center border border-red-500">
-              <input className="border border-gray-300 lg:py-2 lg:px-3 rounded-l-xl" type="text" placeholder="Enter your email"></input>
-              <button className="py-2 px-4 bg rounded-r-xl bg-gradient-to-r from-[#CC6EA6] to-[#E8CB7B]">Subscribe </button>
-            </div>
+      <div className="static bottom-10  w-10/12 md:w-9/12 lg:w-8/12 mx-auto p-2 md:p-3 lg:p-5 text-center rounded-2xl bg-[#00000009] overlay border border-red-600">
+        <div className="p-3 md:p-12 lg:p-24 rounded-2xl bg-gradient-to-tr from-[#CAEDF7] via-[#FFFFFF] to-[#FADCA1] border border-blue-500">
+          <div className="md:8/12 lg::w-1/2 mx-auto text-center rounded-2xl border border-red-600">
+            <h1 className="text-base md:text-xl lg:text-3xl font-bold">Subscribe to our Newsletter</h1>
+            <p className="py-4 text-sm md:text-lg lg:text-xl">Get the latest updates and news right in your inbox!</p>
+            <input className="text-center border border-gray-300 lg:py-2 lg:px-3 lg:rounded-r-xl rounded-xl" type="text" placeholder="Enter your email"></input>
+            <button className="px-14 lg:py-2 lg:px-4 bg lg:rounded-r-xl rounded-xl bg-gradient-to-r from-[#CC6EA6] to-[#E8CB7B]">Subscribe </button>
           </div>
         </div>
       </div>
